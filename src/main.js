63a250b1bcdf49b839914340560e749205608beb
Olimas <ivan.olimas@gmail.com>
2021-06-06 21:07:10 +0300

    06.06.2021

--- src/main.js
+++ src/main.js
@@ -1,14 +1,46 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
 
-Vue.config.productionTip = false
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAnFi_7a4CGh6rsJLGbNjW1dOlYeYoRE7M",
+  authDomain: "crm-vue-1eee7.firebaseapp.com",
+  projectId: "crm-vue-1eee7",
+  storageBucket: "crm-vue-1eee7.appspot.com",
+  messagingSenderId: "59080945894",
+  appId: "1:59080945894:web:eb8f154895779a2b290b27",
+  measurementId: "G-HK0R0Q9G7C"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
+
+
+
+
