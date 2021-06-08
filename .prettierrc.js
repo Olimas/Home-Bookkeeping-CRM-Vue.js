df5e9ad7e235de1f70d78c069da000229aa1cef5
Olimas <ivan.olimas@gmail.com>
2021-06-08 20:53:20 +0300

    08.06.2021

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,5 @@
 module.exports = {
   trallingComma: false,
+  singleQuote: true,
+  // semi: false
 };
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
     <div v-else class="app-main-layout">
       <Navbar @click="isOpen = !isOpen" />
 
-      <Sidebar v-model="isOpen" />
+      <Sidebar v-model="isOpen" :key="locale" />
 
       <main class="app-content" :class="{ full: !isOpen }">
         <div class="app-page">
@@ -26,19 +26,19 @@
 </template>
 
 <script>
-import Navbar from "@/components/app/Navbar";
-import Sidebar from "@/components/app/Sidebar";
-import messages from "@/utils/messages";
+import Navbar from '@/components/app/Navbar';
+import Sidebar from '@/components/app/Sidebar';
+import messages from '@/utils/messages';
 
 export default {
-  name: "main-layout",
+  name: 'main-layout',
   data: () => ({
     isOpen: true,
     loading: true
   }),
   async mounted() {
     if (!Object.keys(this.$store.getters.info).length) {
-      await this.$store.dispatch("fetchInfo");
+      await this.$store.dispatch('fetchInfo');
     }
 
     this.loading = false;
@@ -50,11 +50,14 @@ export default {
   computed: {
     error() {
       return this.$store.getters.error;
+    },
+    locale() {
+      return this.$store.getters.info.locale
     }
   },
   watch: {
     error(fbError) {
-      this.$error(messages[fbError.code] || "Что-то пошло не так");
+      this.$error(messages[fbError.code] || 'Что-то пошло не так');
     }
   }
 };
