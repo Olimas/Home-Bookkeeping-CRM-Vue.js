a50faba511801d2a2b15ea3a94083cb08b499f4d
Olimas <ivan.olimas@gmail.com>
2021-06-08 16:14:20 +0300

    08.06.2021

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
 import messagePlugin from '@/utils/message.plugin'
+import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
 
@@ -16,6 +19,9 @@ Vue.config.productionTip = false
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
 
 firebase.initializeApp({
   apiKey: "AIzaSyAnFi_7a4CGh6rsJLGbNjW1dOlYeYoRE7M",
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import info from './info'
+import category from './category'
+import record from './record'
 
 Vue.use(Vuex)
 
@@ -16,10 +19,17 @@ export default new Vuex.Store({
       state.error = null
     }
   },
+  actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
+      return await res.json()
+    }
+  },
   getters: {
     error: s => s.error
   },
   modules: {
-    auth
+    auth, info, category, record
   }
 })
